102edb87b54bd710618e1884ad6d1f68adf2eee0
sujaltharwani <sujaltharwani493@gmail.com>
2026-07-11 08:52:47 +0530

    Add basic PHP script for age checking

---
+++ 25ESKCY053/DAY7/basic.php
@@ -0,0 +1,23 @@
+// DOWNLOAD OF XAMPP //
+
+
+<?php
+echo "Hi";
+print "Hi";
+print ("Hi");
+$age = 20;
+if($age>=20){
+    print("Adult");
+}else{
+    print("Teenager");
+}
+
+echo "<br>";
+
+$ret = ($age>=20)? print ("Adult") : print ("Teenager");
+
+if(print("true <br>")) {
+}else{
+    print("false <br>")
+}
+?>
