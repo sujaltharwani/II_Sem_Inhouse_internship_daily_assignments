313c1fcb3e0c0af1336425b76c9e1e3343143be7
sujaltharwani <sujaltharwani493@gmail.com>
2026-07-11 09:01:56 +0530

    Add registration form in form.php

---
+++ 25ESKCY053/DAY9/form.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+  
+</head>
+
+<body>
+    <form action = "registration.php" method = "POST">
+      
+        <input type = "text" name = "name" placeholder = "enter your name"> </input>
+        <br>
+        <input type = "email" name = "email" placeholder = "enter your email"> </input>
+        <br>
+        <input type = "text" name = "roll_no" placeholder = "enter your roll number"> </input>
+        <br>
+        <input type = "text" name = "branch" placeholder = "enter your branch"> </input>
+        <br>
+        <input type = "text" name = "cgpa" placeholder = "enter your CGPA"> </input>
+        <br>
+        <input type = "text" name = "phone_no" placeholder = "enter your phone number"> </input>
+        <br>
+      
+        <button type = "Submit">
+            REGISTER
+        </button>
+
+    </form>
+  
+</body>
+</html>
